db45301036d5d4d55921a97293682a89cbec8ff6
devinoneill1 <devinoneill1@gmail.com>
2021-10-11 18:45:29 -0400

    frame out models

--- chorechart_backend/chores/models.py
+++ chorechart_backend/chores/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import datetime
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
+class Task(models.Model):
+    task_name = models.TextField
+    task_day = models.DateField("day of the week repeating",blank=True)
+    repeat = models.BooleanField
+    priority = models.PositiveIntegerField(
+        default=10,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(1)
+            ]
+        )
+    status = models.ForeignKey(
+        'Status',
+        on_delete=models.CASCADE
+    )    
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=200)
